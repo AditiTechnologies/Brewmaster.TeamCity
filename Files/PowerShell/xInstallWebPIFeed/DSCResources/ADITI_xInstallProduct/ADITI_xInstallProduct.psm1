33485e0a30d15c30f7462f3dfc1831b7a53aee23
Brihadish Koushik <brihadishk@aditi.com>
2014-10-22 12:04:21 +0530

    Added configuration to setup environment for building .NET cloud projects...

---
+++ Files/PowerShell/xInstallWebPIFeed/DSCResources/ADITI_xInstallProduct/ADITI_xInstallProduct.psm1
@@ -0,0 +1,117 @@
+﻿#
+# xInstallProduct: DSC resource to install a product via WebPI.
+#
+
+#
+# The Get-TargetResource cmdlet.
+#
+function Get-TargetResource
+{
+	param
+	(	
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ProductTitleOrId
+  	)
+    
+    if(Test-Path "$env:ProgramW6432\Microsoft\Web Platform Installer\WebpiCmd.exe")
+    {
+        $product = Get-Product -SearchOption "Installed" -ProductTitleOrId $ProductTitleOrId
+        $retVal = @{
+            ProductInstalled = !($product -eq $null)
+        }
+    }
+    else
+    {
+        $retVal = @{
+            ProductInstalled = $false;
+            WebPINotAvailable = $true;
+        }
+    }
+}
+
+#
+# The Set-TargetResource cmdlet.
+#
+function Set-TargetResource
+{
+	param
+	(
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ProductTitleOrId
+  	)
+   	
+	Assert-WebPIAvailable
+    $product = Get-Product -SearchOption "Available" -ProductTitleOrId $ProductTitleOrId
+    if(!$product)
+    {
+        throw "Could not find any product with ID or Title matching with $ProductTitleOrId"
+    }
+    Write-Verbose "Installing $ProductTitleOrId"
+    $arg = @( "/Install", "/Products:$ProductTitleOrId", "/AcceptEula", "/SuppressReboot", "/Log:$env:BrewmasterDir\Logs\$ProductTitleOrId.log")
+    & "webpicmd" $arg
+}
+
+#
+# The Test-TargetResource cmdlet.
+#
+function Test-TargetResource
+{
+	param
+	(	
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ProductTitleOrId		
+  	)
+    
+    if(Test-Path "$env:ProgramW6432\Microsoft\Web Platform Installer\WebpiCmd.exe")
+    {
+        $product = Get-Product -SearchOption "Installed" -ProductTitleOrId $ProductTitleOrId
+        if($product)
+        {
+            return $true
+        }
+    }
+    return $false
+}
+
+function Get-Product
+{
+    param
+    (
+        [string]$SearchOption,
+        [string]$ProductTitleOrId
+    )
+    $list = webpicmd /list /ListOption:$SearchOption
+    foreach($entry in $list)
+    {
+        [hashtable]$product = @{}
+        $split = $entry.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
+        if($split.Length -eq 2)
+        {
+            $product.ID = $split[0]
+            $product.Title = $split[1]
+        }
+        elseif($split.Length -eq 3) #The product title itself can have a space
+        {
+            $product.ID = $split[0]
+            $product.Title = [System.String]::Concat($split[1], $split[2])
+        }
+        if(($product.ID -eq $ProductTitleOrId) -or ($product.Title -eq $ProductTitleOrId))
+        {
+            return $product
+        }
+    }
+    return $null
+}
+
+function Assert-WebPIAvailable
+{
+    if(!(Test-Path "$env:ProgramW6432\Microsoft\Web Platform Installer\WebpiCmd.exe"))
+    {
+        throw "Web Platform Installer not available"
+    }
+}
+
+Export-ModuleMember -Function *-TargetResource
---
+++ Files/PowerShell/xInstallWebPIFeed/xInstallWebPIFeed.psd1
@@ -0,0 +1,17 @@
+﻿
+
+@{
+
+
+ModuleVersion = '1.0'
+GUID = '143bacde-9b7f-4efb-99d4-b2d8d433ea3c'
+
+Author = 'Aditi Technologies Pvt Ltd'
+CompanyName = 'Aditi Technologies Pvt Ltd'
+Copyright = '(c) 2014 Aditi Technologies Pvt Ltd. All rights reserved.'
+
+Description = 'Module containing PowerShell Desired State Configuration (DSC) resources that can be used install products available as WebPI feed.'
+
+
+
+}
